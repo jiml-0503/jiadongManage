3fdbc6d48da1b0181d4aefe3d86d697fbba98ce8
jiml-0503 <741240816@qq.com>
2021-02-25 17:50:58 +0800

    把header单独存放一个分支

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,71 +1,18 @@
 <template>
   <div>
-    <div class="header">
-      <div class="collapse-btn">
-        <i class="el-icon-s-unfold"></i>
-      </div>
-      <div class="logo">后台管理系统</div>
-      <div class="header-right">
-        <div class="header-user-con">
-          <div class="user-avator">
-            <img src="@/assets/images/user-avator.jpg" alt="">
-          </div>
-
-          <el-dropdown @command="handleCommand">
-            <span class="el-dropdown-link user-name">
-              {{ $store.state.username }}<i class="el-icon-arrow-down el-icon--right"></i>
-            </span>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item command="a">退出登录</el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-        </div>
-        
-      </div>
-    </div>
+    <home-header></home-header>
   </div>
 </template>
  
 <script>
-//第二种 获取state数据   <span>{{username}}</span>
-// import { mapState } from 'vuex'
+import homeHeader from '@/components/header';
 export default {
+  components: { homeHeader },
   name: 'home',
   data () {
     return {
       
     };
-  },
-  //
-  // computed:{
-  //     ...mapState({
-  //         username:state => state.username   //使用ES6的箭头函数来给count赋值
-  //     })
-  // }
-  created () {
-    // 在页面加载时读取sessionStorage
-    if (sessionStorage.getItem("store")) {
-      this.$store.replaceState(
-        Object.assign(
-          {},
-          this.$store.state,
-          JSON.parse(sessionStorage.getItem("store"))
-        )
-      );
-    }
-    // 在页面刷新时将store保存到sessionStorage里
-    window.addEventListener("beforeunload", () => {
-      sessionStorage.setItem("store", JSON.stringify(this.$store.state));
-    });
-  },
-  methods: {
-    handleCommand(command) {
-      this.$message('click on item ' + command);
-      if(command == 'a'){
-        localStorage.setItem('Authorization','');
-         this.$router.push('/');
-      }
-    }
   }
 };
 </script>
